b1f1d4b84dc03cea60501fa552ba50f0d3a69590
Zavatskii Denis <psyhoravenden@gmail.com>
2013-09-09 09:08:08 +0600

    Echo-server copy-pasta

---
+++ Echo-server/client.py
@@ -0,0 +1,17 @@
+__author__ = 'deniszavatskiy'
+
+import socket
+
+host = "localhost"
+port = 44444
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+while True:
+    buf = raw_input(">>")
+    s.send(buf)
+    result = s.recv(1024)
+    print result
+    if buf == "exit":
+        break
+s.close()
