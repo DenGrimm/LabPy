3db12baeab5c36564b5fdbc3989a04d1649b0b05
Zavatskii Denis <psyhoravenden@gmail.com>
2013-09-16 08:53:13 +0600

    Echo-server copy-pasta

--- Echo-server/__init__.py
+++ Echo-server/__init__.py
@@ -1,28 +1,16 @@
-import asyncore
 import socket
 
-class EchoHandler(asyncore.dispatcher_with_send):
+sock = socket.socket()
+sock.bind(('localhost', 9090))
+sock.listen(1)
+conn, addr = sock.accept()
 
-    def handle_read(self):
-        data = self.recv(8192)
-        if data:
-            self.send(data)
+print 'connected:', addr
 
-class EchoServer(asyncore.dispatcher):
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+    conn.send(data.upper())
 
-    def __init__(self, host, port):
-        asyncore.dispatcher.__init__(self)
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.set_reuse_addr()
-        self.bind((host, port))
-        self.listen(5)
-
-    def handle_accept(self):
-        pair = self.accept()
-        if pair is not None:
-            sock, addr = pair
-            print 'Incoming connection from %s' % repr(addr)
-            handler = EchoHandler(sock)
-
-server = EchoServer('localhost', 8080)
-asyncore.loop()
+conn.close()
